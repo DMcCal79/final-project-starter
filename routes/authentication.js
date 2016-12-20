76527bf5f090f2a984fae54e688f0121f8a95246
DMcCal79 <dave_mccalmont@hotmail.com>
2016-12-20 03:22:52 -0600

    Added first phase of authentication

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,4 +2,66 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jwt-simple');
 const User = require('../models/UserModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
+const passport = require('passport');
+
+//Require our custom strategies
+require('../services/passport');
+
+const signinStrategy = passport.authenticate('signinStrategy', { session: false });
+
+//Helper method that is used to create a token for a user
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ userId: user.id, iat: timestamp }, process.env.SECRET)
+}
+
+router.post('/signin', signinStrategy, function(req, res, next) {
+  res.json({ token: tokenForUser(req.user)});
+});
+
+router.post('/signup', function (req, res, next) {
+  //Takes username & password from request body
+  const { username, password } = req.body;
+
+  //If no username or password was entered then return an error
+  if (!username || !password) {
+    return res.status(422)
+      .json({ error: 'You must provide an username and password' });
+  }
+
+  //Check for a user with the current user name
+  User.findOne({ username }).exec()
+    .then((existingUser) => {
+      //If the user does exist already then return an error on sign up
+      if (existingUser) {
+        return res.status(422).json({ error: 'Username is in use'});
+      }
+
+      //If the user does not already exist then create the user
+      //Use bycrypt to hash their password
+      bcrypt.genSalt(10, function(salt) {
+        bcrypt.hash(password, salt, null, function(err, hashedPassword) {
+          if (err) {
+            return next(err);
+          }
+
+          //Create new user with the supplied username and hashed password
+          const user = new User ({ username, password: hashedPassword});
+
+          //Save and return the user
+          user.save()
+            .then(user => res.json({ token: tokenForUser(user) }));
+        });
+      })
+      .catch(err => next(err));
+    });
+});
+
+const authStrategy = passport.authenticate('authStrategy', { session: false });
+
+router.get('/secret', authStrategy, function(req, res, next) {
+  res.send(`The current user is ${req.user.username}`);
+});
+
+module.exports = router;
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,62 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const passport = require('passport');
 const User = require('../models/UserModel');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const LocalStrategy = require('passport-local');
+
+
+const signinStrategy = new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }).exec()
+    .then(user => {
+      //If no user is found call done with a 'null' argument meaning no error
+      //and 'false' meaning that the sigin failed
+      if (!user) {
+        return done(null, false);
+      }
+
+      bcrypt.compare(password, user.password, function(err, isMatch) {
+      //If there is an error call done with the error
+      if (err) {
+        return done(err, false);
+      }
+
+      //If the passwords do not match call done with a 'null' argument meaning no error
+      //and 'false' meaning that the signin failed
+      if (!isMatch) {
+        return done(null, false);
+      }
+      //If no errors and the passwors match
+      //call done with a 'null' argument meaning no error
+      //and with the now signed in user
+      return done(null, user);
+    });
+  })
+  .catch(err => done(err, false));
+});
+
+//Setup options for JwtStrategy
+const jwtOptions = {
+  //Get the secret from environment
+  secretOrKey: process.env.SECRET,
+  //Tell our strategy where to find the token in the request
+  jwtFromRequest: ExtractJwt.fromHeader('authorization')
+};
+
+//Create JWT strategy
+//This will decode our token to extract the information stored in it
+const authStrategy = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.userId, function(err, user) {
+  if (err) { return done(err, false); }
+
+  if (user) {
+    done(null, user);
+  } else {
+    done(null, false);
+  }
+ });
+});
+
+
+//This tells passport to use this strategy
+passport.use('authStrategy', authStrategy);
+passport.use('signinStrategy', signinStrategy);
